6959b6c42ee400599ae03f28af7194b9735a2ace
Beethoven Kasompe <kasbeet@gmail.com>
2026-01-18 01:01:59 +0200

    particles correction

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Services from "./components/Services";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import ParticlesBackground from "./ParticlesBackground";
+import ParticlesBackground from "./components/ParticlesBackground";
 
 function App() {
   return (
